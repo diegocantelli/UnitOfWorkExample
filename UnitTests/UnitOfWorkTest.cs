6af1fc85abb1132d88def536a11506830639071f
diegocantelli <diegocantelli19@gmail.com>
2023-06-13 10:03:49 -0300

    Adding UnitOfWork tests

--- UnitTests/UnitOfWorkTest.cs
+++ UnitTests/UnitOfWorkTest.cs
@@ -1,10 +1,34 @@
+using UnitOfWorkExample.Interfaces;
+using UnitOfWorkExample.Models;
+using UnitOfWorkExample.Repositories;
+
 namespace UnitTests;
 
-public class UnitTest1
+public class UnitOfWorkTest
 {
-    [Fact]
-    public void Test1()
+    private readonly IUnitOfWork _unitOfWork;
+    public UnitOfWorkTest()
     {
+        _unitOfWork = new UnitOfWork();
+    }
+    
+    [Fact]
+    public void Repositorio_CriaNovoRepositorio_QuandoRepositorioNaoExistir()
+    {
+        var product = _unitOfWork.Repository<Product>();
 
+        Assert.NotNull(product);
+    }
+
+    [Fact]
+    public void Repositorio_RetornarRepositorioExistente_QuandoRepositorioJaExistir()
+    {
+        // arrange
+        var productA = _unitOfWork.Repository<Product>();
+
+        var productB = _unitOfWork.Repository<Product>();
+
+        // Assert
+        Assert.Same(productA, productB);
     }
 }
